642c5109778a20703346ad10f2231ee3addc7676
HARINADHCHANDIKA <146074636+HARINADHCHANDIKA@users.noreply.github.com>
2026-03-24 17:09:22 +0530

    Delete src directory

--- src/predict.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import pandas as pd
-
-model = joblib.load('../model/churn_model.pkl')
-
-def predict(data):
-    df = pd.DataFrame([data])
-    prediction = model.predict(df)
-    return prediction[0]
